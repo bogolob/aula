3136fa27f0f1ae24b54674c96b2f9b28164798d7
Bostjan Golob <79573606+bogolob@users.noreply.github.com>
2024-06-03 16:51:13 +0000

    Create calendar events from EasyIQ week plan

--- custom_components/aula/calendar.py
+++ custom_components/aula/calendar.py
@@ -1,13 +1,18 @@
 from datetime import datetime, timedelta
-import logging, time
-from .const import DOMAIN
-from homeassistant import config_entries, core
-from .const import CONF_SCHOOLSCHEDULE
+import logging
+from typing import Optional
+
+from .const import CONF_SCHOOLSCHEDULE, DOMAIN
+from .client import Client
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.components.calendar import (
     CalendarEntity,
     CalendarEvent,
 )
-from homeassistant.util import Throttle
+from homeassistant.util import Throttle, dt
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,68 +20,73 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=10)
 PARALLEL_UPDATES = 1
 
 async def async_setup_entry(
-    hass: core.HomeAssistant,
-    config_entry: config_entries.ConfigEntry,
-    async_add_entities,
-):
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback
+) -> None:
     config = hass.data[DOMAIN][config_entry.entry_id]
     if config_entry.options:
         config.update(config_entry.options)
-    from .client import Client
+
     if not config[CONF_SCHOOLSCHEDULE] == True:
-        return True
+        return
+
     client = hass.data[DOMAIN]["client"]
     calendar_devices = []
-    calendar = []
-    for i, child in enumerate(client._children):
+
+    for child in client._children:
         childid = child["id"]
         name = child["name"]
-        calendar_devices.append(CalendarDevice(hass,calendar,name,childid))
+        calendar_devices.append(CalendarDevice(hass, name, childid))
+
     async_add_entities(calendar_devices)
 
 class CalendarDevice(CalendarEntity):
-    def __init__(self,hass,calendar,name,childid):
-        self.data = CalendarData(hass,calendar,childid)
-        self._cal_data = {}
-        self._name = "Skoleskema "+name
+    def __init__(self, hass: HomeAssistant, name: str, childid:str):
+        self._name = f"Skoleskema {name}"
+        self._first_name = name.split()[0]
         self._childid = childid
 
+        self.data = CalendarData(hass, self._childid, self._first_name)
+
     @property
-    def event(self):
+    def event(self) -> Optional[CalendarEvent]:
         """Return the next upcoming event."""
         return self.data.event
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the entity."""
         return self._name
 
     @property
-    def unique_id(self):
-        unique_id = "aulacalendar"+str(self._childid)
-        _LOGGER.debug("Unique ID for calendar "+str(self._childid)+" "+unique_id)
+    def unique_id(self) -> str:
+        unique_id = f"aulacalendar{str(self._childid)}"
+        _LOGGER.debug(f"Unique ID for calendar {self._childid} {unique_id}")
         return unique_id
 
-    def update(self):
+    def update(self) -> None:
         """Update all Calendars."""
         self.data.update()
-        
-    async def async_get_events(self, hass, start_date, end_date):
+
+    async def async_get_events(self, hass: HomeAssistant, start_date: datetime, end_date: datetime) -> list[CalendarEvent]:
         """Get all events in a specific time frame."""
-        return await self.data.async_get_events(hass, start_date, end_date)
+        return list(filter(lambda event: event.end > start_date and event.start < end_date, self.data.all_events))
 
 class CalendarData:
-    def __init__(self,hass,calendar,childid):
-        self.event = None
+    def __init__(self, hass: HomeAssistant, childid: str, first_name: str):
+        self.event: Optional[CalendarEvent] = None
 
         self._hass = hass
-        self._calendar = calendar
         self._childid = childid
+        self._first_name = first_name
 
-        self.all_events = []
-        self._client = hass.data[DOMAIN]["client"]
+        self.all_events: list[CalendarEvent] = []
+        self._client: Client = hass.data[DOMAIN]["client"]
 
-    def parseCalendarData(self,i=None):
+    def _parse_skoleskema_json(self) -> list[CalendarEvent]:
+        events: list[CalendarEvent] = []
+
         import json
         try:
             with open('skoleskema.json', 'r') as openfile:
@@ -84,14 +94,14 @@ class CalendarData:
             data = json.loads(_data)
         except:
             _LOGGER.warn("Could not open and parse file skoleskema.json!")
-            return False
-        events = []
+            return events
+
         _LOGGER.debug("Parsing skoleskema.json...")
         for c in data['data']:
             if c['type'] == "lesson" and c['belongsToProfiles'][0] == self._childid:
                 summary = c['title']
-                start = datetime.strptime(c['startDateTime'],"%Y-%m-%dT%H:%M:%S%z")
-                end = datetime.strptime(c['endDateTime'],"%Y-%m-%dT%H:%M:%S%z")
+                start = datetime.strptime(c['startDateTime'],"%Y-%m-%dT%H:%M:%S%z").replace(tzinfo=dt.now().tzinfo)
+                end = datetime.strptime(c['endDateTime'],"%Y-%m-%dT%H:%M:%S%z").replace(tzinfo=dt.now().tzinfo)
                 vikar = 0
                 for p in c['lesson']['participants']:
                     if p['participantRole'] == 'substituteTeacher':
@@ -116,11 +126,51 @@ class CalendarData:
                 events.append(event)
         return events
 
-    async def async_get_events(self, hass, start_date, end_date):
-        events = self.parseCalendarData()
+    @staticmethod
+    def _easyiq_event_to_calendar_event(easyiq_event: dict) -> Optional[CalendarEvent]:
+        start: datetime
+        end: datetime
+
+        try:
+            start = datetime.strptime(easyiq_event["start"], "%Y/%m/%d %H:%M").replace(tzinfo=dt.now().tzinfo)
+        except ValueError:
+            return None
+
+        try:
+            end = datetime.strptime(easyiq_event["end"], "%Y/%m/%d %H:%M").replace(tzinfo=dt.now().tzinfo)
+        except ValueError:
+            return None
+
+        return CalendarEvent(
+                start = start,
+                end = end,
+                description = easyiq_event["description"],
+                summary = f"{easyiq_event["courses"]} - {easyiq_event["activities"]}"
+            )
+
+    def _parse_easyiq_ugep_attr(self, ugep_attr: dict) -> list[CalendarEvent]:
+        if not "Events" in ugep_attr:
+            return []
+
+        return [
+            calendar_event for easyiq_event in ugep_attr["Events"] if (calendar_event := CalendarData._easyiq_event_to_calendar_event(easyiq_event)) is not None
+        ]
+
+    def _parse_easyiq_calendar_events(self) -> list[CalendarEvent]:
+        events: list[CalendarEvent] = []
+
+        if self._client.ugep_attr and self._first_name in self._client.ugep_attr:
+            events += self._parse_easyiq_ugep_attr(self._client.ugep_attr[self._first_name])
+
+        if self._client.ugepnext_attr and self._first_name in self._client.ugepnext_attr:
+            events += self._parse_easyiq_ugep_attr(self._client.ugepnext_attr[self._first_name])
+
         return events
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         _LOGGER.debug("Updating calendars...")
-        self.parseCalendarData(self)
+
+        self.all_events = []
+        self.all_events += self._parse_skoleskema_json()
+        self.all_events += self._parse_easyiq_calendar_events()
