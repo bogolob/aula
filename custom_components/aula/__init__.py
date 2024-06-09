11e2c9fffbd7af3345aaff93ee286f02f2583d39
Bostjan Golob <79573606+bogolob@users.noreply.github.com>
2024-06-09 06:52:44 +0000

    removed dead code

--- custom_components/aula/__init__.py
+++ custom_components/aula/__init__.py
@@ -2,7 +2,6 @@
 Based on https://github.com/JBoye/HA-Aula
 """
 
-import asyncio
 import logging
 from dataclasses import dataclass
 
@@ -71,13 +70,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: AulaConfigEntry) -> bool
 
     integration = await async_get_integration(hass, DOMAIN)
     _LOGGER.info(STARTUP, integration.version)
-    hass.data.setdefault(DOMAIN, {})
-    hass_data = dict(entry.data)
-    # Registers update listener to update config entry when options are updated.
-    unsub_options_update_listener = entry.add_update_listener(options_update_listener)
-    # Store a reference to the unsubscribe function to cleanup if an entry is unloaded.
-    hass_data["unsub_options_update_listener"] = unsub_options_update_listener
-    hass.data[DOMAIN][entry.entry_id] = hass_data
 
     client = Client(
         username,
@@ -108,25 +100,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: AulaConfigEntry) -> bool
     return True
 
 
-async def options_update_listener(
-    hass: HomeAssistant, config_entry: ConfigEntry
-) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(config_entry.entry_id)
-
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[hass.config_entries.async_forward_entry_unload(entry, "sensor")]
-        )
-    )
-    # Remove options_update_listener.
-    hass.data[DOMAIN][entry.entry_id]["unsub_options_update_listener"]()
-
-    # Remove config entry from domain.
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     return unload_ok
--- custom_components/aula/config_flow.py
+++ custom_components/aula/config_flow.py
@@ -86,44 +86,44 @@ class AulaCustomConfigFlow(ConfigFlow, domain=DOMAIN):
 #        return OptionsFlowHandler(config_entry)
 
 
-class OptionsFlowHandler(OptionsFlow):
-    """Blueprint config flow options handler."""
+# class OptionsFlowHandler(OptionsFlow):
+#     """Blueprint config flow options handler."""
 
-    def __init__(self, config_entry: ConfigEntry):
-        """Initialize HACS options flow."""
-        self.config_entry = config_entry
-        self.options = dict(config_entry.options)
+#     def __init__(self, config_entry: ConfigEntry):
+#         """Initialize HACS options flow."""
+#         self.config_entry = config_entry
+#         self.options = dict(config_entry.options)
 
-    async def async_step_init(
-        self, user_input: Optional[dict[str, Any]] = None
-    ) -> ConfigFlowResult:
-        """Manage the options."""
-        _LOGGER.debug("Options......")
-        _LOGGER.debug(self.config_entry)
-        entity_registry = async_get(self.hass)
-        entries = async_entries_for_config_entry(
-            entity_registry, self.config_entry.entry_id
-        )
-        repo_map = {e.entity_id: e for e in entries}
-        for entity_id in repo_map.keys():
-            # Unregister from HA
-            _LOGGER.debug(entity_id)
-            # entity_registry.async_remove(entity_id)
-        return await self.async_step_user()
+#     async def async_step_init(
+#         self, user_input: Optional[dict[str, Any]] = None
+#     ) -> ConfigFlowResult:
+#         """Manage the options."""
+#         _LOGGER.debug("Options......")
+#         _LOGGER.debug(self.config_entry)
+#         entity_registry = async_get(self.hass)
+#         entries = async_entries_for_config_entry(
+#             entity_registry, self.config_entry.entry_id
+#         )
+#         repo_map = {e.entity_id: e for e in entries}
+#         for entity_id in repo_map.keys():
+#             # Unregister from HA
+#             _LOGGER.debug(entity_id)
+#             # entity_registry.async_remove(entity_id)
+#         return await self.async_step_user()
 
-    async def async_step_user(
-        self, user_input: Optional[dict[str, Any]] = None
-    ) -> ConfigFlowResult:
-        """Handle a flow initialized by the user."""
-        if user_input is not None:
-            self.options.update(user_input)
-            return await self._update_options()
+#     async def async_step_user(
+#         self, user_input: Optional[dict[str, Any]] = None
+#     ) -> ConfigFlowResult:
+#         """Handle a flow initialized by the user."""
+#         if user_input is not None:
+#             self.options.update(user_input)
+#             return await self._update_options()
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=AUTH_SCHEMA,
-        )
+#         return self.async_show_form(
+#             step_id="user",
+#             data_schema=AUTH_SCHEMA,
+#         )
 
-    async def _update_options(self) -> ConfigFlowResult:
-        """Update config entry options."""
-        return self.async_create_entry(title="Aula", data=self.options)
+#     async def _update_options(self) -> ConfigFlowResult:
+#         """Update config entry options."""
+#         return self.async_create_entry(title="Aula", data=self.options)
