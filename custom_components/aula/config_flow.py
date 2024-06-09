d8cfdc4e58f3ae6dce189de62fea3d7fabbb6845
Bostjan Golob <79573606+bogolob@users.noreply.github.com>
2024-06-09 13:30:29 +0000

    Reconfiguration

--- custom_components/aula/config_flow.py
+++ custom_components/aula/config_flow.py
@@ -43,7 +43,39 @@ class AulaCustomConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
     MINOR_VERSION = 1
 
+    _config_entry: Optional[ConfigEntry] = None
     data: Optional[dict[str, Any]]
+
+    def _process_user_input(self, user_input: dict[str, Any]) -> ConfigFlowResult:
+        self.data = user_input
+
+        _LOGGER.debug(self.data.get(CONF_SCHOOLSCHEDULE))
+        if self.data.get(CONF_SCHOOLSCHEDULE) is None:
+            self.data[CONF_SCHOOLSCHEDULE] = False
+
+        _LOGGER.debug(self.data.get(CONF_UGEPLAN))
+        if self.data.get(CONF_UGEPLAN) is None:
+            self.data[CONF_UGEPLAN] = False
+
+        _LOGGER.debug(self.data.get(CONF_PARSE_EASYIQ_UGEPLAN))
+        if self.data.get(CONF_PARSE_EASYIQ_UGEPLAN) is None:
+            self.data[CONF_PARSE_EASYIQ_UGEPLAN] = False
+
+        _LOGGER.debug(self.data.get(CONF_EASYIQ_UGEPLAN_CALENDAR))
+        if self.data.get(CONF_EASYIQ_UGEPLAN_CALENDAR) is None:
+            self.data[CONF_EASYIQ_UGEPLAN_CALENDAR] = False
+
+        # This will log password in plain text: _LOGGER.debug(self.data)
+
+        if self._config_entry:
+            return self.async_update_reload_and_abort(
+                self._config_entry,
+                title="Aula",
+                data=self.data,
+                reason="reconfigure_successful",
+            )
+
+        return self.async_create_entry(title="Aula", data=self.data)
 
     async def async_step_user(
         self, user_input: Optional[dict[str, Any]] = None
@@ -52,30 +84,33 @@ class AulaCustomConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input is not None:
-            self.data = user_input
-
-            _LOGGER.debug(self.data.get(CONF_SCHOOLSCHEDULE))
-            if self.data.get(CONF_SCHOOLSCHEDULE) is None:
-                self.data[CONF_SCHOOLSCHEDULE] = False
-
-            _LOGGER.debug(self.data.get(CONF_UGEPLAN))
-            if self.data.get(CONF_UGEPLAN) is None:
-                self.data[CONF_UGEPLAN] = False
-
-            _LOGGER.debug(self.data.get(CONF_PARSE_EASYIQ_UGEPLAN))
-            if self.data.get(CONF_PARSE_EASYIQ_UGEPLAN) is None:
-                self.data[CONF_PARSE_EASYIQ_UGEPLAN] = False
-
-            _LOGGER.debug(self.data.get(CONF_EASYIQ_UGEPLAN_CALENDAR))
-            if self.data.get(CONF_EASYIQ_UGEPLAN_CALENDAR) is None:
-                self.data[CONF_EASYIQ_UGEPLAN_CALENDAR] = False
-
-            # This will log password in plain text: _LOGGER.debug(self.data)
-
-            return self.async_create_entry(title="Aula", data=self.data)
+            return self._process_user_input(user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=AUTH_SCHEMA, errors=errors, last_step=True
+            step_id="user",
+            data_schema=AUTH_SCHEMA,
+            errors=errors,
+            last_step=True,
+        )
+
+    async def async_step_reconfigure(
+        self, user_input: Optional[dict[str, Any]] = None
+    ) -> ConfigFlowResult:
+        self._config_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+
+        assert self._config_entry
+
+        if user_input:
+            return self._process_user_input(user_input)
+
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=self.add_suggested_values_to_schema(
+                AUTH_SCHEMA, self._config_entry.data
+            ),
+            last_step=True,
         )
 
 
